fbb2dd7a93020553fc759a29f7bad086f2ed6f47
Shivansh Chauhan <myoffice2706@gmail.com>
2025-06-06 14:47:50 +0530

    login, dashboard, auth. added

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import register_view, dashboard_view, home_view
+
+urlpatterns = [
+    path('', home_view, name='home'),
+    path('register/', register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .forms import UserregistrationForm
+from .models import Device
+
+
+def home_view(request):
+    return render(request, 'core/home.html')
+
+def register_view(request):
+    if request.method == 'POST':
+         form = UserregistrationForm(request.POST)
+         if form.is_valid():
+              form.save()
+              return redirect('login')
+    else:
+         form = UserregistrationForm()
+    return render(request, 'core/register.html',{'form':form})
+
+
+@login_required
+def dashboard_view(request):
+     devices = request.user.devices.all()
+     return render(request, 'core/dashboard.html', {'devices': devices})
+
+         
 
 # Create your views here.
