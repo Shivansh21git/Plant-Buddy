a2761688b5c141c076c7e7729506db3aceb2b541
Shivansh Chauhan <myoffice2706@gmail.com>
2025-06-06 11:04:30 +0530

    db created with superuser & user table

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Device(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='devices')
+    device_id = models.CharField(max_length=100,unique=True)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+
+
+    def __self__(self):
+        return f"{self.name} ({self.device_id})"
