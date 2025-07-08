7c0304c4549afaac42d2e41d7120c41257e9b7cf
Shivansh Chauhan <myoffice2706@gmail.com>
2025-07-08 11:20:18 +0530

    add-device-button added and currently without docker layer

--- core/views.py
+++ core/views.py
@@ -59,7 +59,7 @@ def device_data_view(request, device_id):
 
     query = f'''
     from(bucket: "{INFLUX_BUCKET}")
-      |> range(start: -7d)
+      |> range(start: -20d)
       |> filter(fn: (r) => r["_measurement"] == "npk_data")
       |> filter(fn: (r) => r["device_id"] == "{device_id}")
       |> last()
