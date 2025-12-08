8cbf153c88a78ec6a631ae456bbc8ff09199cc39
Shivansh Chauhan <myoffice2706@gmail.com>
2025-12-08 14:23:25 +0530

    working-main

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-x-jcs!gm%rwsp4o75hba)-(4!8dlmo6br41lc3842&-11pus4(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["plant-buddy-production.up.railway.app"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -146,5 +146,6 @@ MQTT_HOST = os.getenv('MQTT_BROKER')
 MQTT_TOPIC = os.getenv('MQTT_PUB_TOPIC')
 MQTT_PORT  = os.getenv('BROKER_PORT')
 
-CSRF_TRUSTED_ORIGINS = [ "http://plant-buddy-production.up.railway.app", "https://plant-buddy-production.up.railway.app"
+CSRF_TRUSTED_ORIGINS = [
+    "https://ar.bhoomitra.space",  "https://dev.bhoomitra.space", "https://plant-buddy-production.up.railway.app"
 ]
